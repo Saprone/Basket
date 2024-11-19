b3f2816ff0c5be6e0e6a48ed9bebc34490b2c858
Bas <onrust.bas@gmail.com>
2024-11-19 17:27:54 +0100

    Initial commit

---
+++ src/main/java/com/saprone/userregistrationsender/UserregistrationsenderApplication.java
@@ -0,0 +1,13 @@
+package com.saprone.userregistrationsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserregistrationsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserregistrationsenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saprone/userregistrationsender/UserregistrationsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saprone.userregistrationsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserregistrationsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
