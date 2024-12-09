b3b1030e1c6e20da4f4cce759120483f7353c834
Bas <onrust.bas@gmail.com>
2024-12-09 22:37:57 +0100

    seperated controller and service

--- src/main/java/com/saprone/basket/controller/BasketController.java
+++ src/main/java/com/saprone/basket/controller/BasketController.java
@@ -1,41 +1,29 @@
 package com.saprone.basket.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.saprone.basket.model.Basket;
-import com.saprone.basket.repository.BasketRepository;
+import com.saprone.basket.service.BasketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 @RestController
 @RequestMapping("/basket")
 @CrossOrigin(origins = "http://localhost:8085")
 public class BasketController {
 
-    private final BasketRepository basketRepository;
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final BasketService basketService;
 
     @Autowired
-    public BasketController(BasketRepository basketRepository) {
-        this.basketRepository = basketRepository;
+    public BasketController(BasketService basketService) {
+        this.basketService = basketService;
     }
 
     @PostMapping("/ingredient")
     public ResponseEntity<Void> addIngredientToBasket(@RequestBody String encodedBasket) {
-        String decodedBasket = URLDecoder.decode(encodedBasket, StandardCharsets.UTF_8);
-
         try {
-            List<Basket> baskets = objectMapper.readValue(decodedBasket.substring(0, decodedBasket.length() - 1), new TypeReference<List<Basket>>() {});
-
-            basketRepository.saveAll(baskets);
+            basketService.addIngredientsToBasket(encodedBasket);
         } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
-
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/saprone/basket/service/BasketService.java
@@ -0,0 +1,29 @@
+package com.saprone.basket.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.saprone.basket.model.Basket;
+import com.saprone.basket.repository.BasketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Service
+public class BasketService {
+
+    private final BasketRepository basketRepository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    public BasketService(BasketRepository basketRepository) {
+        this.basketRepository = basketRepository;
+    }
+
+    public void addIngredientsToBasket(String encodedBasket) throws Exception {
+        String decodedBasket = URLDecoder.decode(encodedBasket, StandardCharsets.UTF_8);
+        List<Basket> baskets = objectMapper.readValue(decodedBasket.substring(0, decodedBasket.length() - 1), new TypeReference<List<Basket>>() {});
+        basketRepository.saveAll(baskets);
+    }
+}
