03a2ece5c2bd8c2df1b1a749e9ffeb39a38d7697
Bas <onrust.bas@gmail.com>
2024-12-09 17:44:57 +0100

    Update BasketController.java

--- src/main/java/com/saprone/basket/controller/BasketController.java
+++ src/main/java/com/saprone/basket/controller/BasketController.java
@@ -1,5 +1,8 @@
 package com.saprone.basket.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.net.URLDecoder;
@@ -11,8 +14,11 @@ import java.nio.charset.StandardCharsets;
 public class BasketController {
 
     @PostMapping("/post")
-    public ResponseEntity<String> postData(@RequestBody String encodedBasket) {
+    public ResponseEntity<String> postData(@RequestBody String encodedBasket) throws JsonProcessingException {
         String decodedBasket = URLDecoder.decode(encodedBasket, StandardCharsets.UTF_8);
+        JsonNode jsonNode = new ObjectMapper().readTree(decodedBasket);
+
+        jsonNode.forEach(System.out::println);
 
         return ResponseEntity.ok("Post request succeeded with data: " + decodedBasket);
     }
